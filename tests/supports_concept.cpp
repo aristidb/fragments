bc2a24ac1009f97d71e7111ad6a03a98f9a83055
kingruedi <kingruedi@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-13 17:38:17 +0000

    added first headers
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@11 75736e42-b832-0410-8f05-b18d7a61c935

---
+++ tests/supports_concept.cpp
@@ -0,0 +1,23 @@
+#include <fragments/concepts/utils/supports_concept.hpp>
+#include <boost/mpl/vector.hpp>
+#include <iostream>
+
+namespace {
+  struct conceptA { };
+
+  struct policyA {
+    template<typename Before, typename After>
+    struct policy : public Before { };
+
+    typedef boost::mpl::vector1<conceptA> concept;
+  };
+}
+
+int main() {
+  std::cout <<
+    fragments::concepts::utils::supports_concept<
+     policyA,
+     conceptA
+       >::value
+            << '\n';
+}
