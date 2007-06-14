75b38df02683560e69d2aea24608512499dbc815
el_mrn <el_mrn@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-14 14:37:44 +0000

    untested new functionality
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@38 75736e42-b832-0410-8f05-b18d7a61c935

---
+++ include/fragments/as_concept_each.hpp
@@ -0,0 +1,57 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+
+// Copyright (c) 2007 Aristid Breitkreuz, Ruediger Sonderfeld
+//
+// Distributed under the Boost Software License, Version 1.0 and under the MIT
+// License.
+//
+// See accompanying LICENSES.txt or
+// <http://fragments.sourceforge.net/LICENSES.txt> or, for the Boost Software
+// License, <http://www.boost.org/LICENSE_1_0.txt>.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#ifndef FRAGMENTS_AS_CONCEPT_EACH_HPP
+#define FRAGMENTS_AS_CONCEPT_EACH_HPP
+
+#include <fragments/filter_concept.hpp>
+#include <boost/mpl/for_each.hpp>
+
+namespace fragments {
+
+namespace detail {
+  template<typename P, typename Fun>
+  struct invoker {
+    invoker(P ptr, Fun fun) : ptr(ptr), fun(fun) {}
+
+    P ptr;
+    Fun fun;
+
+    template<typename Fragment>
+    void operator() (Fragment const &) {
+      fun(as_fragment<Fragment>(*ptr));
+    }
+  };
+}
+
+template<typename C, typename H, typename F>
+void as_concept_each(H &h, F fun) {
+  typedef filter_concept_lazy<typename H::fragments, C>::type filtered;
+  boost::mpl::for_each<filtered>(detail::invoker(&h, fun));
+}
+
+template<typename C, typename H, typename F>
+void as_concept_each(H const &h, F fun) {
+  typedef filter_concept_lazy<typename H::fragments, C>::type filtered;
+  boost::mpl::for_each<filtered>(detail::invoker(&h, fun));
+}
+
+}
+
+#endif
---
+++ include/fragments/filter_concept.hpp
@@ -0,0 +1,42 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+
+// Copyright (c) 2007 Aristid Breitkreuz, Ruediger Sonderfeld
+//
+// Distributed under the Boost Software License, Version 1.0 and under the MIT
+// License.
+//
+// See accompanying LICENSES.txt or
+// <http://fragments.sourceforge.net/LICENSES.txt> or, for the Boost Software
+// License, <http://www.boost.org/LICENSE_1_0.txt>.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#ifndef FRAGMENTS_FILTER_CONCEPT_HPP
+#define FRAGMENTS_FILTER_CONCEPT_HPP
+
+#include <fragments/concepts/supports_concept.hpp>
+#include <boost/mpl/filter_view.hpp>
+#include <boost/mpl/placeholder.hpp>
+
+namespace fragments {
+
+template<typename FragmentSeq, typename Concept>
+struct filter_concept_lazy {
+  typedef typename boost::mpl::filter_view<
+      FragmentSeq,
+      concepts::supports_concept<
+        boost::mpl::_1,
+        Concept
+      >
+    >::type type;
+}
+
+}
+
+#endif
