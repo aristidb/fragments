dcbcea39c82c5d6bb0b95656c432687daf345960
kingruedi <kingruedi@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-14 13:09:54 +0000

    added license header
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@33 75736e42-b832-0410-8f05-b18d7a61c935

--- tests/flatten-valid.cpp
+++ tests/flatten-valid.cpp
@@ -1,3 +1,22 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+
+// Copyright (c) 2007 Aristid Breitkreuz, Ruediger Sonderfeld
+//
+// Distributed under the Boost Software License, Version 1.0 and under the MIT
+// License.
+//
+// See accompanying LICENSES.txt or
+// <http://fragments.sourceforge.net/LICENSES.txt> or, for the Boost Software
+// License, <http://www.boost.org/LICENSE_1_0.txt>.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
 #include <fragments/concepts/flatten.hpp>
 
 #include <boost/preprocessor/stringize.hpp>
