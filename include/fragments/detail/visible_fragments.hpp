2616c23e10e46864c7266e77a13ab350b70687ed
el_mrn <el_mrn@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-28 22:15:00 +0000

    added visible_fragments metafct
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@83 75736e42-b832-0410-8f05-b18d7a61c935

---
+++ include/fragments/detail/visible_fragments.hpp
@@ -0,0 +1,78 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+
+// Copyright (c) 2007 Aristid Breitkreuz, Ruediger Sonderfeld
+//
+// Distributed under the Boost Software License, Version 1.0 and under the MIT
+// License.
+//
+// See accompanying LICENSES.txt or
+// <http://fragments.sourceforge.net/LICENSES.txt> or, for the Boost Software
+// License, <http://www.boost.org/LICENSE_1_0.txt>.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#ifndef FRAGMENTS_DETAIL_VISIBLE_FRAGMENTS_HPP
+#define FRAGMENTS_DETAIL_VISIBLE_FRAGMENTS_HPP
+
+#include <boost/mpl/has_xxx.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/deref.hpp>
+#include <boost/mpl/eval_if.hpp>
+#include <boost/mpl/begin.hpp>
+#include <boost/mpl/end.hpp>
+#include <boost/mpl/next_prior.hpp>
+#include <boost/mpl/iterator_range.hpp>
+
+namespace fragments { namespace detail {
+
+BOOST_MPL_HAS_XXX_TRAIT_DEF(shadow)
+
+template<typename T, bool = has_shadow<T>::value>
+struct test_shadow
+  : T::shadow
+{};
+
+template<typename T>
+struct test_shadow<T, false>
+  : boost::mpl::false_
+{};
+
+template<
+  typename Seq,
+  typename First = typename boost::mpl::begin<Seq>::type,
+  typename Last = typename boost::mpl::end<Seq>::type
+>
+struct visible_fragments {
+  typedef typename boost::mpl::deref<First>::type first;
+
+  typedef typename boost::mpl::next<First>::type next;
+
+  typedef typename boost::mpl::eval_if<
+      test_shadow<first>,
+      visible_fragments<
+        boost::mpl::iterator_range<First, Last>,
+        next,
+        Last
+      >,
+      visible_fragments<
+        Seq,
+        next,
+        Last
+      >
+    >::type type;
+};
+
+template<typename Seq, typename End>
+struct visible_fragments<Seq, End, End> {
+  typedef Seq type;
+};
+
+}}
+
+#endif
---
+++ tests/visible_fragments-valid.cpp
@@ -0,0 +1,38 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+
+// Copyright (c) 2007 Aristid Breitkreuz, Ruediger Sonderfeld
+//
+// Distributed under the Boost Software License, Version 1.0 and under the MIT
+// License.
+//
+// See accompanying LICENSES.txt or
+// <http://fragments.sourceforge.net/LICENSES.txt> or, for the Boost Software
+// License, <http://www.boost.org/LICENSE_1_0.txt>.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#include <fragments/detail/visible_fragments.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/static_assert.hpp>
+#include <typeinfo>
+#include <iostream>
+
+struct f0 { typedef boost::mpl::true_ shadow; };
+struct f1 {};
+struct f2 { typedef boost::mpl::true_ shadow; };
+struct f3 { typedef boost::mpl::false_ shadow; };
+struct f4 {};
+
+int main() {
+  typedef boost::mpl::vector5<f0, f1, f2, f3, f4> seq;
+  typedef fragments::detail::visible_fragments<seq>::type vis;
+  BOOST_STATIC_ASSERT((boost::mpl::size<vis>::value == 3));
+  //std::cout << "_Z1x" << typeid(vis).name() << std::endl;
+}
