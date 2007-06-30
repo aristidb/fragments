b4bc53096f8012587aaf278adf0bafec6ff97fac
realisticer <realisticer@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-30 17:49:48 +0000

    * added vector.cpp
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@93 75736e42-b832-0410-8f05-b18d7a61c935

--- include/fragments/container/vector.hpp
+++ include/fragments/container/vector.hpp
@@ -32,6 +32,41 @@ namespace fragments { namespace container {
 
     template<typename Before, typename>
     struct fragment : Before {
+      typedef std::vector<typename Before::value_type> Seq;
+      
+      typedef typename Before::value_type value_type;
+      typedef typename Before::reference reference;
+      typedef typename Before::const_reference const_reference;
+      typedef typename Seq::size_type size_type;
+
+      Seq sequence;
+
+      fragment() {
+      }
+
+      bool empty() const {
+        return sequence.empty();
+      }
+
+      size_type size() const {
+        return sequence.size();
+      }
+
+      reference back() {
+        return sequence.back();
+      }
+
+      const_reference back() const {
+        return sequence.back();
+      }
+
+      void push_back(const_reference vt) {
+        sequence.push_back(vt);
+      }
+
+      void pop_back() {
+        sequence.pop_back();
+      }
     };
   };
 }}
