42a6a9f034ecf4e98434d2a505ac26a8f06e2a39
el_mrn <el_mrn@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-30 23:28:32 +0000

    improved supports_concept for VC 2005 compatibility
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@99 75736e42-b832-0410-8f05-b18d7a61c935

--- include/fragments/concepts/supports_concept.hpp
+++ include/fragments/concepts/supports_concept.hpp
@@ -1,3 +1,5 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+
 // Copyright (c) 2007 Aristid Breitkreuz, Ruediger Sonderfeld, Christian Uhrhan
 //
 // Distributed under the Boost Software License, Version 1.0 and under the MIT
@@ -30,26 +32,38 @@ namespace fragments {
 namespace concepts {
 
 namespace detail {
-  template<typename FragmentSeq, typename Concept,
-           bool empty = boost::mpl::empty<FragmentSeq>::value>
+  template<
+    typename FragmentSeq,
+    typename Concept,
+    bool empty = boost::mpl::empty<FragmentSeq>::value
+  >
+  struct checker;
+
+  template<
+    typename Concept,
+    typename CheckedConcept,
+    bool = has_implies<CheckedConcept>::value
+  >
+  struct implies_checker {
+    static bool const value = checker<
+        typename CheckedConcept::implies,
+        Concept
+      >::value;
+  };
+
+  template<typename Concept, typename CheckedConcept>
+  struct implies_checker<Concept, CheckedConcept, false> {
+    static bool const value = false;
+  };
+
+  template<typename FragmentSeq, typename Concept, bool /*false*/>
   struct checker {
     typedef typename boost::mpl::front<FragmentSeq>::type checked_concept;
-
-    template<typename CheckedConcept, bool = has_implies<CheckedConcept>::value>
-    struct implies {
-      static bool const value =
-        checker<typename CheckedConcept::implies, Concept>::value;
-    };
-
-    template<typename CheckedConcept>
-    struct implies<CheckedConcept, false> {
-      static bool const value = false;
-    };
 
     static bool const value =
       boost::is_same<checked_concept, Concept>::value ?
       true :
-      ( implies<checked_concept>::value ?
+      ( implies_checker<Concept, checked_concept>::value ?
         true :
         checker<
           typename boost::mpl::pop_front<FragmentSeq>::type,
