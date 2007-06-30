f2145a75650842e0708a1065dc06ae1850d1a914
el_mrn <el_mrn@75736e42-b832-0410-8f05-b18d7a61c935>
2007-06-30 16:46:57 +0000

    realisticer's work on stack
    git-svn-id: https://fragments.svn.sourceforge.net/svnroot/fragments/trunk@87 75736e42-b832-0410-8f05-b18d7a61c935

--- include/fragments/container/stack.hpp
+++ include/fragments/container/stack.hpp
@@ -20,7 +20,62 @@
 #ifndef FRAGMENTS_CONTAINER_STACK_HPP
 #define FRAGMENTS_CONTAINER_STACK_HPP
 
+#include <fragments/container/concepts/container.hpp>
+#include <fragments/container/concepts/type_provider.hpp>
+#include <fragments/container/vector.hpp>
+
 namespace fragments { namespace container {
+  struct stack {
+      typedef boost::mpl::true_ shadow;
+      typedef boost::mpl::vector0<> concept;//für stack haben wir (noch?) kein eigenes concept
+      typedef boost::mpl::vector2<
+          concepts::container,
+          concepts::type_provider
+        > require_before;
+      typedef boost::mpl::map1<
+          boost::mpl::pair<concepts::container, vector>
+        > default_fragment;
+
+      template<typename Before, typename>
+      struct fragment {
+          typedef typename Before::access access;
+          // type_provider types
+          typedef typename Before::value_type value_type;
+          typedef typename Before::reference reference;
+          typedef typename Before::const_reference const_reference;
+          // container types
+          typedef typename Before::size_type size_type;
+        private:
+          Before sequence;
+        public:
+          fragment() {
+          }
+
+          bool empty() const {
+            return sequence.empty();
+          }
+
+          size_type size() const {
+            return sequence.size();
+          }
+
+          reference top() {
+            return sequence.back();
+          }
+
+          const_reference top() const {
+            return sequence.back();
+          }
+
+          void push(const_reference vt) {
+            sequence.push_back(vt);
+          }
+
+          void pop() {
+            sequence.pop_back();
+          }
+      }; //end fragment
+  };//end stack
 }}
 
 #endif
